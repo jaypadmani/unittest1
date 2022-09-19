8e8770c6e586200e15e8b432311a3802aa7ca21a
Jay_1407 <88769189+jaypadmani@users.noreply.github.com>
2022-09-19 13:48:31 +0530

    Update App.js

--- studentmanagementsystembackend-master/src/main/webapp/src/App.js
+++ studentmanagementsystembackend-master/src/main/webapp/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Patient from './components/Patient';
 import PatientList from './components/PatientList';
 import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
