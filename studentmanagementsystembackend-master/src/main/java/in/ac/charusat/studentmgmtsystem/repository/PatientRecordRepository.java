25c189c8767575184f3fc44decbf11e255238932
Jay_1407 <88769189+jaypadmani@users.noreply.github.com>
2022-09-19 13:31:12 +0530

    Update PatientRecordRepository.java

--- studentmanagementsystembackend-master/src/main/java/in/ac/charusat/studentmgmtsystem/repository/PatientRecordRepository.java
+++ studentmanagementsystembackend-master/src/main/java/in/ac/charusat/studentmgmtsystem/repository/PatientRecordRepository.java
@@ -3,5 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PatientRecordRepository extends JpaRepository <Patient,Integer> {
 }
